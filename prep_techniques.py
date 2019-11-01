be79ad0b03ba6ba9c0988a7a042e1bbf4dc61e28
GabijaNag <gabija.nag@gmail.com>
2019-11-01 19:40:03 +0200

    Update prep_techniques.py

--- prep_techniques.py
+++ prep_techniques.py
@@ -1,15 +1,150 @@
-import re
+import re, unicodedata, unidecode
 from functools import partial
+from bs4 import BeautifulSoup
 import nltk
-from nltk.corpus import wordnet
+from nltk.corpus import wordnet, stopwords
+from nltk.stem import LancasterStemmer, WordNetLemmatizer
+import inflect
 
 
-#######################################
-## Replacing Negations with Antonyms ##
-#######################################
+##################
+## Remove Noise ##
+##################
+
+def remove_html(text):
+    """Strips away HTML markup"""
+    soup = BeautifulSoup(text, 'lxml')
+    html_free = soup.get_text()
+    return html_free
+
+
+def remove_between_square_brackets(text):
+    """Removes open and close double brackets and anything in between them"""
+    return re.sub('\[[^]]*\]', '', text)
+
+
+def remove_accented_chars(text):
+    """Converts accented characters from text, e.g. café to cafe"""
+    text = unidecode.unidecode(text)
+    return text
+
+
+def remove_punctuation(words):
+    """Removes punctuation from a list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = re.sub(r'[^\w\s]', '', word)
+        if new_word != '':
+            new_words.append(new_word)
+    return new_words
+
+def remove_non_ascii(words):
+    """Removes non-ASCII characters from a list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+        new_words.append(new_word)
+    return new_words
+
+
+###################
+## Deduplication ##
+###################
+
+def remove_dupes(text):
+    """Removes duplicate rows"""
+    seen = set()
+    result = []
+    for item in text:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    return result
+
+
+###################
+## Normalization ##
+###################
+
+def to_lowercase(words):
+    """Converts all characters to lowercase from a list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = word.lower()
+        new_words.append(new_word)
+    return new_words
+
+
+def replace_numbers(words):
+    """Replaces all interger occurrences in list of tokenized words with textual representation"""
+    p = inflect.engine()
+    number_counter = 0
+    new_words = []
+    for word in words:
+        if word.isdigit():
+            number_counter += 1
+            new_word = p.number_to_words(word)
+            new_words.append(new_word)
+        else:
+            new_words.append(word)
+    print(f"Converted {number_counter} numbers to words.")
+    return new_words
+
+
+def remove_numbers(words):
+    """Strips digits from a list of tokenized words"""
+    no_digits = []
+    for i in words:
+        if not i.isdigit():
+            no_digits.append(i)
+    result = ''.join(no_digits)
+    return result
+
+
+def remove_stopwords(words):
+    """Removes stop words from list of tokenized words"""
+    new_words = []
+    for word in words:
+        if word not in stopwords.words('english'):
+            new_words.append(word)
+    return new_words
+
+
+def stem_words(words):
+    """Stems words in list of tokenized words"""
+    stemmer = LancasterStemmer()
+    stems = []
+    for word in words:
+        stem = stemmer.stem(word)
+        stems.append(stem)
+    return stems
+
+
+def lemmatize_words(words):
+    """Lemmatizes verbs in list of tokenized words"""
+    lemmatizer = WordNetLemmatizer()
+    lemmas = []
+    for word in words:
+        lemma = lemmatizer.lemmatize(word, pos='v')
+        lemmas.append(lemma)
+    return lemmas
+
+
+def stem_and_lemmatize(words):
+    """Creates and prints a list of stemmed and lemmatized verbs"""
+    stems = stem_words(words)
+    lemmas = lemmatize_words(words)
+    print('Stemmed:\n', stems)
+    print('\nLemmatized:\n', lemmas)
+    return stems, lemmas
+
+
+#####################################
+## Replace Negations with Antonyms ##
+#####################################
 
 def replace(word, pos=None):
-    """ Create a set of all antonyms for the word and if there is only one antonym, it returns it """
+    """Creates a set of all antonyms for the word and if there is only one antonym, it returns it """
     antonyms = set()
     for syn in wordnet.synsets(word, pos=pos):
         for lemma in syn.lemmas():
@@ -20,8 +155,20 @@ def replace(word, pos=None):
     else:
         return None
 
+
+def detect_negations(text):
+    """Counts how many negations are found and displays a list of them"""
+    negation_counter = 0
+    negations_found = []
+    for word in text:
+        if "'" in word:
+            negations_found.append(word)
+            negation_counter += 1
+    return negation_counter, negations_found
+
+
 def replace_negations(text):
-    """ Replace "not" and the next word with the antonym """
+    """Replaces "not" and the next word with the antonym """
     i, l = 0, len(text)
     words = []
     while i < l:
@@ -45,8 +192,9 @@ with open('slang.txt') as file:
     slang_map = dict(map(str.strip, line.partition('\t')[::2])
     for line in file if line.strip())
 
-def count_slang(text):
-    """ Input: string of text, Output: how many slang words and a list of found slangs """
+
+def detect_slang(text):
+    """Input: string of text, Output: how many slang words and a list of found slangs """
     slangCounter = 0
     slangsFound = []
     tokens = nltk.word_tokenize(text)
@@ -55,6 +203,7 @@ def count_slang(text):
             slangsFound.append(word)
             slangCounter += 1
     return slangCounter, slangsFound
+
 
 slang_words = sorted(slang_map, key=len, reverse=True) # longest first for regex
 regex = re.compile(r"\b({})\b".format("|".join(map(re.escape, slang_words))))
@@ -90,12 +239,12 @@ cList = {
   "how'd'y": "how do you",
   "how'll": "how will",
   "how's": "how is",
-  "I'd": "I would",
-  "I'd've": "I would have",
-  "I'll": "I will",
-  "I'll've": "I will have",
-  "I'm": "I am",
-  "I've": "I have",
+  "i'd": "i would",
+  "i'd've": "i would have",
+  "i'll": "i will",
+  "i'll've": "i will have",
+  "i'm": "i am",
+  "i've": "i have",
   "isn't": "is not",
   "it'd": "it had",
   "it'd've": "it would have",
@@ -185,10 +334,22 @@ cList = {
   "you're": "you are",
   "you've": "you have"
 }
-
 c_re = re.compile('(%s)' % '|'.join(cList.keys()))
 
+
+def count_contractions(text):
+    """Counts how many contractions are there and displays a list of them"""
+    contractions_counter = 0
+    contractions_found = []
+    for word in text:
+        if "'" in word:
+            contractions_found.append(word)
+            contractions_counter += 1
+    return contractions_counter, contractions_found
+
+
 def expand_contractions(text, c_re=c_re):
+    """Replaces contractions with their expansions"""
     def replace(match):
         return cList[match.group(0)]
     return c_re.sub(replace, text)
